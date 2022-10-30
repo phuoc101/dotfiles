a1e68720d4c315c0eb86b22d8768204a2cf10117
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-10-30 10:59:26 +0200

    move to configs

--- update.zsh
+++ update.zsh
@@ -4,19 +4,19 @@ trash ./.zshrc
 cp -vir ~/.zsh zsh
 cp -vir ~/.zshrc .zshrc
 # update i3
-trash ./i3
-trash ./i3blocks
-cp -vir ~/.config/i3 i3
-cp -vir ~/.config/i3blocks i3blocks
+trash ./.config/i3
+trash ./.config/i3blocks
+cp -vir ~/.config/i3 ./.config/i3
+cp -vir ~/.config/i3blocks ./.config/i3blocks
 # update rofi
-trash ./rofi
-cp -vir ~/.config/rofi rofi
+trash ./.config/rofi
+cp -vir ~/.config/rofi ./.config/rofi
 # update dunst
-trash ./dunst
-cp -vir ~/.config/dunst dunst
+trash ./.config/dunst
+cp -vir ~/.config/dunst ./.config/dunst
 # update kitty
-trash ./kitty
-cp -vir ~/.config/kitty kitty
+trash ./.config/kitty
+cp -vir ~/.config/kitty ./.config/kitty
 # update picom
-trash ./picom
-cp -vir ~/.config/picom picom
+trash ./.config/picom
+cp -vir ~/.config/picom ./.config/picom
