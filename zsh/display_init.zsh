86eedace5e9432d6916358dbccd7e89dd6b93e36
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-11-09 12:06:18 +0200

    turn off picom cus too much RAM hogging, add work2K display profile

--- zsh/display_init.zsh
+++ zsh/display_init.zsh
@@ -13,6 +13,11 @@ elif [[ $XRANDR_OUT == *"HDMI-0 connected 2560x1440"* ]] || [[ $XRANDR_OUT == *"
   xrandr --output HDMI-0 --primary
   feh --bg-scale $MY_WALLPAPER --bg-scale $MY_WALLPAPER
   notify-send "Initialized display" "work"
+elif [[ $XRANDR_OUT == *"HDMI-0 connected 3440x1440"* ]] || [[ $XRANDR_OUT == *"HDMI-0 connected primary 3440x1440"* ]]; then
+  autorandr --load work2K
+  xrandr --output HDMI-0 --primary
+  feh --bg-scale $MY_WALLPAPER --bg-scale $MY_WALLPAPER
+  notify-send "Initialized display" "work2K"
 elif [[ $XRANDR_OUT == *"HDMI-0 disconnected"* ]]; then
   autorandr --load mobile
   xrandr --output eDP-1-1 --primary
@@ -22,3 +27,4 @@ fi
 notify-send "Updating betterlockscreen" "$MY_WALLPAPER"
 betterlockscreen -u $MY_WALLPAPER
 notify-send "betterlockscreen updated" "$MY_WALLPAPER"
+
