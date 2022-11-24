7e4b695156de0f3b8011abebcdfcef76e5e8d299
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-11-24 15:29:43 +0200

    added link script

---
+++ link.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+trash ~/.config/aliasrc
+trash ~/.config/alacritty
+trash ~/.config/dunst
+trash ~/.config/i3
+trash ~/.config/polybar
+trash ~/.config/rofi
+trash ~/.zshrc
+trash ~/.zsh
+trash ~/.local/bin/dark_mode
+trash ~/.local/bin/light_mode
+trash ~/.tmux.conf
+
+ln -s ~/dotfiles/.config/aliasrc ~/.config/aliasrc
+ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/.config/dunst ~/.config/dunst
+ln -s ~/dotfiles/.config/i3 ~/.config/i3
+ln -s ~/dotfiles/.config/polybar ~/.config/polybar
+ln -s ~/dotfiles/.config/rofi ~/.config/rofi
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zsh ~/.zsh
+ln -s ~/dotfiles/dark_mode ~/.local/bin/dark_mode
+ln -s ~/dotfiles/light_mode ~/.local/bin/light_mode
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
