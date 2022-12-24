166f6daa3e9f734b114b0bc1ecad0d6d95d88357
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2022-12-24 17:59:55 +0700

    updated link script

--- link.zsh
+++ link.zsh
@@ -1,22 +1,27 @@
-#!/bin/bash
+#!/bin/zsh
 trash ~/.config/aliasrc
 trash ~/.config/alacritty
 trash ~/.config/dunst
 trash ~/.config/i3
 trash ~/.config/polybar
 trash ~/.config/rofi
+trash ~/.config/kitty
+trash ~/.config/nvim
 trash ~/.zshrc
 trash ~/.zsh
 trash ~/.local/bin/dark_mode
 trash ~/.local/bin/light_mode
 trash ~/.tmux.conf
 
+# Link config files
 ln -s ~/dotfiles/.config/aliasrc ~/.config/aliasrc
 ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
 ln -s ~/dotfiles/.config/dunst ~/.config/dunst
 ln -s ~/dotfiles/.config/i3 ~/.config/i3
 ln -s ~/dotfiles/.config/polybar ~/.config/polybar
 ln -s ~/dotfiles/.config/rofi ~/.config/rofi
+ln -s ~/dotfiles/.config/kitty ~/.config/kitty
+ln -s ~/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.zsh ~/.zsh
 ln -s ~/dotfiles/dark_mode ~/.local/bin/dark_mode
