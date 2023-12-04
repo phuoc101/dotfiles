a9a7e0b2336713fb7e241d1db1178a0c1c4e358f
Phuoc Nguyen <phuoc.nguyen@aalto.fi>
2023-12-04 09:54:33 +0200

    update launch script for polybar

--- .config/polybar/scripts/launch.sh
+++ .config/polybar/scripts/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 sleep 2
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
@@ -9,14 +9,14 @@ polybar-msg cmd quit
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 if xrandr | grep -q 'HDMI-1-0 connected'; then
-  polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
-  polybar sub 2>&1 | tee -a /tmp/polybar2.log & disown
-  notify-send "Polybar" "Launched main and sub bars"
+    polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+    polybar sub 2>&1 | tee -a /tmp/polybar2.log & disown
+    notify-send "Polybar" "Launched main and sub bars"
 elif xrandr | grep -q 'DP-1-4 connected'; then
-  polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
-  polybar sub 2>&1 | tee -a /tmp/polybar2.log & disown
-  notify-send "Polybar" "Launched main and sub bars"
+    polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+    polybar sub 2>&1 | tee -a /tmp/polybar2.log & disown
+    notify-send "Polybar" "Launched main and sub bars"
 else
-  polybar main_2k 2>&1 | tee -a /tmp/polybar1.log & disown
-  notify-send "Polybar" "Launched main bar"
+    polybar main_2k 2>&1 | tee -a /tmp/polybar1.log & disown
+    notify-send "Polybar" "Launched main bar"
 fi
