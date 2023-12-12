20196e32202bfa5c4e91217392ea96ba896f9132
Phuoc Nguyen <phuoc.nguyen@aalto.fi>
2023-12-12 16:19:59 +0200

    update vimrc

--- .zsh/zshenvs.zsh
+++ .zsh/zshenvs.zsh
@@ -2,8 +2,8 @@
 export PATH=$PATH:/home/phuoc101/.local/bin:/opt/gradle/gradle-6.3/bin
 export PATH=/usr/lib/ccache:$PATH
 export PATH=~/go/bin:/usr/local/go/bin:$PATH
-export PATH=/usr/local/cuda-11.3/bin:$PATH
-# export PATH=/usr/lib/cuda-11.2/bin:$PATH
+export CUDA_VER=11.8
+export PATH=/usr/local/cuda-${CUDA_VER}/bin:$PATH
 export EDITOR='nvim'
 export VISUAL='nvim'
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/media/phuoc101/imaunicorn/Projects/robot_proj_2022/turtlebot3_ws/src/turtlebot3/turtlebot3_simulations/turtlebot3_gazebo/models
@@ -11,10 +11,8 @@ export GTK_IM_MODULE=ibus
 export QT4_IM_MODULE=xim
 export QT_IM_MODULE=xim
 export XMODIFIERS=@im=ibus
-export CPATH=/usr/local/cuda-11.3/targets/x86_64-linux/include:$CPATH
-# export CPATH=/usr/lib/cuda-11.2/targets/x86_64-linux/include:$CPATH
-export LD_LIBRARY_PATH=/usr/local/cuda-11.3/targets/x86_64-linux/lib:/usr/local/cuda-11.3/lib64:$LD_LIBRARY_PATH
-# export LD_LIBRARY_PATH=/usr/lib/cuda-11.2/targets/x86_64-linux/lib:/usr/lib/cuda-11.2/lib64:$LD_LIBRARY_PATH
+export CPATH=/usr/local/cuda-${CUDA_VER}/targets/x86_64-linux/include:$CPATH
+export LD_LIBRARY_PATH=/usr/local/cuda-${CUDA_VER}/targets/x86_64-linux/lib:/usr/local/cuda-${CUDA_VER}/lib64:$LD_LIBRARY_PATH
 ZSH_DISABLE_COMPFIX=true
 # kitty themes
 export KITTY_DARK_THEME=Afterglow
@@ -28,4 +26,21 @@ export MASTERS_DIR=/media/phuoc101/imaunicorn/masters
 export PROJECTS_DIR=/media/phuoc101/imaunicorn/projects
 export OBSIDIAN_DIR=/media/phuoc101/imaunicorn/backup/obsidian
 export ALACRITTY_DIR=/home/phuoc101/.config/alacritty
+conceptgraphs_envs() {
+    export CG_FOLDER=/home/phuoc101/irobotics/projects/ml_prj/vlm/conceptgraphs
 
+    export AM_I_DOCKER=False
+    export BUILD_WITH_CUDA=True
+    export CUDA_HOME=/usr/local/cuda-${CUDA_VER}
+
+    export GSA_PATH=${CG_FOLDER}/Grounded-Segment-Anything
+
+    export LLAVA_PYTHON_PATH=${CG_FOLDER}/LLaVA/llava
+    export LLAVA_MODEL_PATH=${CG_FOLDER}/LLaVA/LLaVA-7b-delta-v0
+
+    export REPLICA_ROOT=${CG_FOLDER}/Datasets/Replica
+    export REPLICA_CONFIG_PATH=${CG_FOLDER}/concept-graphs/conceptgraph/dataset/dataconfigs/replica/replica.yaml
+}
+# AALTO SERVERS
+export GUNDAM_IP=130.233.123.198
+export MARVIN_IP=130.233.123.196
