330424ff7d58f4eb42f97829f21264734eb01488
Phuoc Nguyen <nguyenthuanphuoc101@protonmail.com>
2025-01-11 21:07:10 +0200

    update kitty tab bar

---
+++ .config/kitty/tab_bar.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from kitty.boss import get_boss
+from kitty.fast_data_types import Screen, add_timer
+from kitty.rgb import to_color
+from kitty.tab_bar import (
+    DrawData,
+    ExtraData,
+    Formatter,
+    TabBarData,
+    as_rgb,
+    draw_attributed_string,
+    draw_tab_with_powerline,
+)
+
+REFRESH_TIME = 1
+
+def _redraw_tab_bar(_) -> None:
+    tm = get_boss().active_tab_manager
+    if tm is not None:
+        tm.mark_tab_bar_dirty()
+
+
+timer_id = None
+
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_title_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    global timer_id
+    if timer_id is None:
+        timer_id = add_timer(_redraw_tab_bar, REFRESH_TIME, True)
+    draw_tab_with_powerline(
+        draw_data, screen, tab, before, max_title_length, index, is_last, extra_data
+    )
+    return screen.cursor.x
